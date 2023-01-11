67fd9cc10c32ad7c3ba0a36f2510ceb79365a5ed
ibusyadzali <ibnusyadzali@gmail.com>
2023-01-11 23:58:42 +0700

    fetchOrderDetail and authorization done

--- controllers/OrderControllers.js
+++ controllers/OrderControllers.js
@@ -4,14 +4,33 @@ class OrderControllers {
     static async createOrder(req,res,next) {
         try {
             const UserId = req.user.id
-            const {shoesBrand, shoesSize, shoesColor, shoesMaterial, phoneNumberPIC, photo, pickUpAddress, ServiceId} = req.body
+            let {shoesBrand, shoesSize, shoesColor, shoesMaterial, phoneNumberPIC, photo, pickUpAddress, ServiceId} = req.body
             
             const orderIdNumber = `#${UserId}${ServiceId}${Math.floor(Math.random() * 100)}`
+        
+            const noPicture = "../assets/Sorry... there's no image yet (.png"
 
-            const data = await Order.create({orderIdNumber, shoesBrand, shoesSize, shoesColor, shoesMaterial, phoneNumberPIC, photo, pickUpAddress, status: 'On Request ', ServiceId, UserId})
+            if (!photo) {
+                photo = noPicture
+            }
+            let photoAfter = noPicture
+            const data = await Order.create({orderIdNumber, shoesBrand, shoesSize, shoesColor, shoesMaterial, phoneNumberPIC, photo, pickUpAddress, photoAfter, status: 'On Request ', ServiceId, UserId})
             
             res.status(201).json({ message: `Success create order with Order ID Number ${data.orderIdNumber}`})
         } catch (error) {
+            next(error)
+        }
+    }
+    static async fetchOrderDetail(req, res, next) {
+        try {
+            const orderId = req.params.orderId
+            const data = await Order.findOne({where: {id : orderId}, include: [User, Service]})
+            if (!data) {
+                throw {name: 'Data not found'}
+            }
+            res.status(200).json(data)
+        } catch (error) {
+            console.log(error)
             next(error)
         }
     }
--- middleware/authorization.js
+++ middleware/authorization.js
@@ -1,6 +1,24 @@
-function authorization(req,res,next) {
-    return
+const { Order } = require("../models");
 
+async function authorization(req, res, next) {
+  try {
+      let id = req.params.orderId;
+      console.log(id)
+    let orderData = await Order.findByPk(id);
+    if (!orderData) {
+      throw { name: `Data not found` };
+    } else {
+      let userId = req.user.id;
+      if (orderData.UserId === userId) {
+        next();
+      } else {
+        throw { name: `Unauthorized` };
+      }
+    }
+  } catch (error) {
+    console.log(error);
+    next(error);
+  }
 }
 
-module.exports= authorization
+module.exports = authorization;
