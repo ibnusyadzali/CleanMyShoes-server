df0d5e2bdbd4a63beac573082b192933851ccace
ibusyadzali <ibnusyadzali@gmail.com>
2023-01-11 22:42:58 +0700

    create order done + athentication

--- controllers/OrderControllers.js
+++ controllers/OrderControllers.js
@@ -1,7 +1,22 @@
 const {User, Service, Order} = require('../models/index')
 
 class OrderControllers {
+    static async createOrder(req,res,next) {
+        try {
+            const UserId = req.user.id
+            const {shoesBrand, shoesSize, shoesColor, shoesMaterial, phoneNumberPIC, photo, pickUpAddress, ServiceId} = req.body
+            
+            const orderIdNumber = `#${UserId}${ServiceId}${Math.floor(Math.random() * 100)}`
 
+            const data = await Order.create({orderIdNumber, shoesBrand, shoesSize, shoesColor, shoesMaterial, phoneNumberPIC, photo, pickUpAddress, status: 'On Request ', ServiceId, UserId})
+            
+            res.status(201).json({ message: `Success create order with Order ID Number ${data.orderIdNumber}`})
+        } catch (error) {
+            console.log(error)
+            next(error)
+        }
+
+    }
 }
 
 module.exports = OrderControllers
--- routers/ordersRouter.js
+++ routers/ordersRouter.js
@@ -1,9 +1,9 @@
 const express = require('express')
 const OrderControllers = require("../controllers/OrderControllers")
-const { authentication, authorization } = require('../middleware/authentication')
+const authentication = require('../middleware/authentication')
 const router = express.Router()
 
-// router.post('/', authentication,OrderControllers.createOrder)
+router.post('/', authentication,OrderControllers.createOrder)
 // router.get('/', authentication,OrderControllers.readMyOrder)
 // router.get('/:id', authentication, authorization,OrderControllers.readMyOrderDetail)
 // router.put('/:id', authentication, authorization,OrderControllers.updateOrderStatus)
